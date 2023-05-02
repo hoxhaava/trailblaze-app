f20441a97f57781db610338df73458e6d79f83d5
hoxhahava <hoxha.ava.mak@gmail.com>
2023-05-02 20:25:19 +0300

    setting up a map

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native"
+import { StyleSheet, SafeAreaView } from "react-native"
+import { Text } from "react-native-elements"
+import Map from "../components/Map"
 
 const TrackCreateScreen = () => {
     return (
-        <View>
-            <Text style={{ fontSize: 48 }}>Track Create Screen</Text>
-        </View>)
+        <SafeAreaView forceInset={{ top: 'always'}}>
+            <Text h2>Create a Track</Text>
+            <Map />
+        </SafeAreaView>)
 }
 
 const styles = StyleSheet.create({})
