49130bbc8634cf23f962309a1a108c1da342bf7d
hoxhahava <hoxha.ava.mak@gmail.com>
2023-05-05 16:26:17 +0300

    save track info in db

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export default axios.create({
+const instance =  axios.create({
     baseURL: 'https://ff22-95-111-127-174.ngrok-free.app'
 })
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config;
+     },
+    (err) => {
+        return Promise.reject(err)
+     }
+)
+
+export default instance
